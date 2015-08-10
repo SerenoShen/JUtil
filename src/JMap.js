019a2fb2ea45398c4c15f0d881e2704d719bfbe6
shenlei <shenlei2@yonyou.com>
2015-08-10 13:58:20 +0800

    修改JMap的具体实现方式，修复之前实现对闭包应用产生的错误

--- src/JMap.js
+++ src/JMap.js
@@ -1,69 +1,77 @@
 var JMap = (function (){
-	
-	var 
-		size = 0, 
-		map = {},
-		strundefined;
-	
+
 	function MapCache() {
+
+		this.size = 0;
+		this.mapCache = {};
 
 	}
 
-	MapCache.prototype.put = function (key, value){
-		
-		if ( key === null && key === strundefined ){
-			throw new Error( " null or undefined is not a illegality variable ! " );
-		}
-		
-		if (map[ key + " " ] == strundefined) {
-			map[ key + " "] = value;
-			size++;
-			return;
-		}
-		
-		map[ key + " " ] = value;
-	};
+	MapCache.prototype = {
 
-	MapCache.prototype.remove = function (key) {
+		put : function (key, value){
 
-		var value;
+			if ( key === null && key === undefined ){
+				throw new Error( " null or undefined is not a illegality variable ! " );
+			}
 
-		if ( key !== null || key !== strundefined ) {
-			value = map[ key + " " ];
-			delete map[ key + " " ];
-			size--;
+			if (this.mapCache[ key + " " ] == undefined) {
+				this.mapCache[ key + " "] = value;
+				this.size++;
+				return;
+			}
+
+			this.mapCache[ key + " " ] = value;
+		},
+
+		get : function (key) {
+			if (this.mapCache[key + " "] != undefined) {
+				return this.mapCache[ key + " " ];
+			}
+
+			return null;
+		},
+
+		remove : function (key) {
+
+			var value;
+
+			if ( key !== null || key !== undefined ) {
+				value = this.mapCache[ key + " " ];
+				delete this.mapCache[ key + " " ];
+				this.size--;
+				return value;
+			}
+
 			return value;
+		},
+
+		clear : function () {
+			this.mapCache = {};
+			this.size = 0;
+		},
+
+		keys : function () {
+			var keys = [], prop;
+
+			for ( prop in this.mapCache ){
+				keys.push( prop.substring(0, prop.length - 1) );
+			}
+
+			return keys;
+		},
+
+		size : function () {
+			return this.size;
 		}
-
-		return value;
 	};
 
-	MapCache.prototype.get = function (key) {
-		if (map[key + " "] != strundefined) {
-			return map[ key + " " ];
-		}
-		
-		return null;
-	};
-
-	MapCache.prototype.keys = function () {
-		var keys = [], prop;
-
-		for ( prop in map ){
-			keys.push(prop);
-		}
-
-		return keys;
-	};
-	
-	MapCache.prototype.clear = function () {
-		map = {};
-		size = 0;
-	};
-	
-	MapCache.prototype.size = function () {
-		return size;
-	};
+	// 遵循 AMD 模块化规范， 支持RequireJS 模块化导出
+	if ( typeof define === 'function' && define.amd ) {
+		define('jmap', [], function (){
+			return MapCache;
+		})
+	}
 	
 	return MapCache;
 }());
